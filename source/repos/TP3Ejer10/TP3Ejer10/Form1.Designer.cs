6f64c1ec40ba2ba1de9f6b2ceb7f75039a6089f4
Matias-NBKMANTO <mezequielc@MANTOVANA.local>
2023-09-08 17:32:48 -0300

    Commit 17.32hs 08/09

---
+++ source/repos/TP3Ejer10/TP3Ejer10/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace TP3Ejer10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericValor = new System.Windows.Forms.NumericUpDown();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbInicial = new System.Windows.Forms.CheckedListBox();
+            this.lbFinal = new System.Windows.Forms.CheckedListBox();
+            this.lblRespuesta = new System.Windows.Forms.Label();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericValor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericValor
+            // 
+            this.numericValor.Location = new System.Drawing.Point(243, 68);
+            this.numericValor.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericValor.Name = "numericValor";
+            this.numericValor.Size = new System.Drawing.Size(78, 22);
+            this.numericValor.TabIndex = 1;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(132, 70);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(105, 17);
+            this.lbl1.TabIndex = 2;
+            this.lbl1.Text = "Ingrese el valor";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(370, 47);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(154, 17);
+            this.lbl2.TabIndex = 7;
+            this.lbl2.Text = "Ingrese la escala inicial";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(568, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 17);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Ingrese a que escalar convertir";
+            // 
+            // lbInicial
+            // 
+            this.lbInicial.FormattingEnabled = true;
+            this.lbInicial.Items.AddRange(new object[] {
+            "Celsius",
+            "Fahrenheit",
+            "Kelvin",
+            "Rankine"});
+            this.lbInicial.Location = new System.Drawing.Point(373, 70);
+            this.lbInicial.Name = "lbInicial";
+            this.lbInicial.Size = new System.Drawing.Size(119, 72);
+            this.lbInicial.TabIndex = 13;
+            this.lbInicial.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.lbInicial_ItemCheck);
+            // 
+            // lbFinal
+            // 
+            this.lbFinal.FormattingEnabled = true;
+            this.lbFinal.Items.AddRange(new object[] {
+            "Celsius",
+            "Fahrenheit",
+            "Kelvin",
+            "Rankine"});
+            this.lbFinal.Location = new System.Drawing.Point(571, 70);
+            this.lbFinal.Name = "lbFinal";
+            this.lbFinal.Size = new System.Drawing.Size(121, 72);
+            this.lbFinal.TabIndex = 14;
+            this.lbFinal.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.lbFinal_ItemCheck);
+            // 
+            // lblRespuesta
+            // 
+            this.lblRespuesta.AutoSize = true;
+            this.lblRespuesta.Location = new System.Drawing.Point(240, 264);
+            this.lblRespuesta.Name = "lblRespuesta";
+            this.lblRespuesta.Size = new System.Drawing.Size(0, 17);
+            this.lblRespuesta.TabIndex = 15;
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.Location = new System.Drawing.Point(326, 163);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(130, 50);
+            this.btnConvertir.TabIndex = 16;
+            this.btnConvertir.Text = "CONVERTIR";
+            this.btnConvertir.UseVisualStyleBackColor = true;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConvertir);
+            this.Controls.Add(this.lblRespuesta);
+            this.Controls.Add(this.lbFinal);
+            this.Controls.Add(this.lbInicial);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.numericValor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericValor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericValor;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox lbInicial;
+        private System.Windows.Forms.CheckedListBox lbFinal;
+        private System.Windows.Forms.Label lblRespuesta;
+        private System.Windows.Forms.Button btnConvertir;
+    }
+}
+
---
+++ source/repos/TP3Ejer10/TP3Ejer10/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP3Ejer10
+{
+    public partial class Form1 : Form
+    {
+        string opcionSeleccionada = "";
+        string opcionSeleccionadaFinal = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvertir_Click(object sender, EventArgs e)
+        {
+            int temperaturaInicial = (int)numericValor.Value;
+            
+
+        }
+
+        private void lbInicial_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            // Desmarca todas las demás opciones
+            for (int i = 0; i < lbInicial.Items.Count; i++)
+            {
+                if (i != e.Index)
+                {
+                    lbInicial.SetItemCheckState(i, CheckState.Unchecked);
+                                opcionSeleccionada = lbInicial.GetItemText(lbInicial.Items[e.Index]);
+
+                }
+            }
+            opcionSeleccionada = lbInicial.GetItemText(lbInicial.Items[e.Index]);
+
+
+
+        }
+
+        private void lbFinal_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            // Desmarca todas las demás opciones
+            for (int i = 0; i < lbFinal.Items.Count; i++)
+            {
+                if (i != e.Index)
+                {
+                    lbFinal.SetItemCheckState(i, CheckState.Unchecked);
+
+                }
+            }
+            if (opcionSeleccionada!="")
+            {
+                opcionSeleccionadaFinal = lbFinal.GetItemText(lbFinal.Items[e.Index]);
+
+            }
+            else
+            {
+
+            }
+
+        }
+    }
+}
